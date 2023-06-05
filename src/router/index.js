7c1da66e2687769186f010cf2f13d8393de6b376
Shubh-343 <shubhamstpss@gmail.com>
2023-06-05 07:41:57 +0530

    testing

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import TodosView from '../views/TodosView.vue'
+import TodosView from '../views/TodosViews.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
